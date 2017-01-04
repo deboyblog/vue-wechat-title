2351b1abc8983cc72085cce3b2f55b38ab532401
Deboy <55631825@qq.com>
2017-01-04 23:26:35 +0800

    修复网页闪烁的问题

--- vue-wechat-title.js
+++ vue-wechat-title.js
@@ -1,35 +1,43 @@
 (function () {
-  var setWechatTitle = function (title) {
-    document.title = title
-    var mobile = navigator.userAgent.toLowerCase()
-    if (/iphone|ipad|ipod/.test(mobile)) {
-      var iframe = document.createElement('iframe')
-      iframe.style.visibility = 'hidden'
-      // 替换成站标favicon路径或者任意存在的较小的图片即可
-      iframe.setAttribute('src', '/static/logo.png')
-      var iframeCallback = function () {
-        setTimeout(function () {
-          iframe.removeEventListener('load', iframeCallback)
-          document.body.removeChild(iframe)
-        }, 0)
-      }
-      iframe.addEventListener('load', iframeCallback)
-      document.body.appendChild(iframe)
+    function install (Vue) {
+        var setWechatTitle = function (title) {
+            if (title === undefined) {
+                return
+            }
+            document.title = title
+            var mobile = navigator.userAgent.toLowerCase()
+            if (/iphone|ipad|ipod/.test(mobile)) {
+                var iframe = document.createElement('iframe')
+                iframe.style.display = 'none'
+                // 替换成站标favicon路径或者任意存在的较小的图片即可
+                iframe.setAttribute('src', '/favicon.ico')
+                var iframeCallback = function () {
+                    setTimeout(function () {
+                        iframe.removeEventListener('load', iframeCallback)
+                        document.body.removeChild(iframe)
+                    }, 0)
+                }
+                iframe.addEventListener('load', iframeCallback)
+                document.body.appendChild(iframe)
+            }
+        }
+        Vue.directive('wechat-title', {
+            bind: function (title) {
+                setWechatTitle(title)
+            },
+            update: function (newTitle) {
+                setWechatTitle(newTitle)
+            }
+        })
     }
-  }
-  function install (Vue) {
-    Vue.directive('wechat-title', {
-      bind: setWechatTitle,
-      update: setWechatTitle
-    })
-  }
 
-  if (typeof exports === 'object') {
-    module.exports = install
-  } else if (typeof define === 'function' && define.amd) {
-    define([], function(){ return install })
-  } else if (window.Vue) {
-    Vue.use(install)
-  }
-
+    if (typeof exports === 'object') {
+        module.exports = install
+    } else if (typeof define === 'function' && define.amd) {
+        define([], function () {
+            return install
+        })
+    } else if (window.Vue) {
+        Vue.use(install)
+    }
 })()
