4f3fdf2a10bf1faf994b77fdd4d357919ef25dfd
Deboy <55631825@qq.com>
2017-03-14 14:34:37 +0800

    closes #5

--- vue-wechat-title.js
+++ vue-wechat-title.js
@@ -1,38 +1,39 @@
 (function () {
-    function install (Vue) {
-        var setWechatTitle = function (title) {
-            if (title === undefined) {
-                return
-            }
-            document.title = title
-            var mobile = navigator.userAgent.toLowerCase()
-            if (/iphone|ipad|ipod/.test(mobile)) {
-                var iframe = document.createElement('iframe')
-                iframe.style.display = 'none'
-                // 替换成站标favicon路径或者任意存在的较小的图片即可
-                iframe.setAttribute('src', '/favicon.ico')
-                var iframeCallback = function () {
-                    setTimeout(function () {
-                        iframe.removeEventListener('load', iframeCallback)
-                        document.body.removeChild(iframe)
-                    }, 0)
-                }
-                iframe.addEventListener('load', iframeCallback)
-                document.body.appendChild(iframe)
-            }
+  function install (Vue) {
+    var setWechatTitle = function (title, img) {
+      if (title === undefined) {
+        return
+      }
+      console.log(title, img)
+      document.title = title
+      var mobile = navigator.userAgent.toLowerCase()
+      if (/iphone|ipad|ipod/.test(mobile)) {
+        var iframe = document.createElement('iframe')
+        iframe.style.display = 'none'
+        // 替换成站标favicon路径或者任意存在的较小的图片即可
+        iframe.setAttribute('src', img || '/favicon.ico')
+        var iframeCallback = function () {
+          setTimeout(function () {
+            iframe.removeEventListener('load', iframeCallback)
+            document.body.removeChild(iframe)
+          }, 0)
         }
-        Vue.directive('wechat-title', function (el, binding) {
-            setWechatTitle(binding.value)
-        })
+        iframe.addEventListener('load', iframeCallback)
+        document.body.appendChild(iframe)
+      }
     }
+    Vue.directive('wechat-title', function (el, binding) {
+      setWechatTitle(binding.value, el.getAttribute('img-set') || null)
+    })
+  }
 
-    if (typeof exports === 'object') {
-        module.exports = install
-    } else if (typeof define === 'function' && define.amd) {
-        define([], function () {
-            return install
-        })
-    } else if (window.Vue) {
-        Vue.use(install)
-    }
+  if (typeof exports === 'object') {
+    module.exports = install
+  } else if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return install
+    })
+  } else if (window.Vue) {
+    Vue.use(install)
+  }
 })()
